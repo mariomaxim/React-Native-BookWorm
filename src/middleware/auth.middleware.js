b704bb3bbfa8f18c67af446a52d491da6f30cc91
Mario Maxim <121346160+mariomaxim@users.noreply.github.com>
2025-04-26 12:49:20 +0300

    Update auth.middleware.js

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import User from '../models/user.js';
+import User from '../models/User.js';
 
 // const response =  await fetch(`http://localhost:3000/api/books`,{
 //     method: 'POST',
